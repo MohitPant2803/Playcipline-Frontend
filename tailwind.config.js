a360863e01d58169e6b903d628f87d53d47dc519
MohitPant2803 <mohi13245@gmail.com>
2026-05-06 15:14:22 +0530

    changes by cline

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export default {
   content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
@@ -12,18 +11,3 @@ export default {
   },
   plugins: []
 };
-=======
-export default {
-  content: ['./index.html', './src/**/*.{js,jsx}'],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#3b82f6',
-        secondary: '#10b981',
-        danger: '#ef4444'
-      }
-    }
-  },
-  plugins: []
-};
->>>>>>> 7b034b46f0cd1ce3de12072ccfde4a1f592202b9
